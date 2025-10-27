82897c760dcfa86edaf0303b9fb4062085a65967
Mateus Mendes <mateusmendesdefreitas12@gmail.com>
2025-10-27 18:15:56 +0000

    Adicionando icone na fly

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-     
+     URL::forceRootUrl(config('app.url'));
+     if(str_starts_with(config('app.url'), 'https://')){
+         URL::forceScheme('https');
+     }
     }
 }
