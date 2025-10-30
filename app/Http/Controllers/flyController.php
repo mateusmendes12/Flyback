5c0183f9a396d284f33c103a4ece35c5270f3ecf
Mateus Mendes <mateusmendesdefreitas12@gmail.com>
2025-10-30 00:17:57 +0000

    Versão original devolta

--- app/Http/Controllers/flyController.php
+++ app/Http/Controllers/flyController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Fly;
+use Illuminate\Support\Facades\DB;
 
 class flyController extends Controller
 {
@@ -11,20 +12,39 @@ class flyController extends Controller
      * Display a listing of the resource.
      */
     public function index(Request $request)
+
+    
     {
-        $flies = Fly::paginate(10)->appends($request->except('page'));
-        //retorna também os parâmetros de busca na 
-        // paginação
-        // $pages=[];
-        // for($page=1; $page <= $flies->lastPage(); $page++){
-        //     $pages[]=$page;
+        $flies=Fly::paginate(10);
+      
+        // //retorna também os parâmetros de busca na 
+        // // paginação
+        // // $pages=[];
+        // // for($page=1; $page <= $flies->lastPage(); $page++){
+        // //     $pages[]=$page;
 
-        // }
+        // // }
 
-        // pode retornar quantos likes e deslikes uma fly tem
+        // // pode retornar quantos likes e deslikes uma fly tem
 
-         // usa query builder
+        //  // usa query builder
          
+        return view('flies.index', compact('flies'));
+// $flies = DB::table('flies')
+//     ->join('fly_votes', 'flies.id', '=', 'fly_votes.fly_id')
+//     ->select(
+//         'flies.id',
+//         'flies.title',
+//         'flies.description',  // Incluindo 'description'
+//         'flies.created_at',
+//         'flies.updated_at',
+//         DB::raw('SUM(CASE WHEN fly_votes.type_vote = "like" THEN 1 ELSE 0 END) as likes_count'),
+//         DB::raw('SUM(CASE WHEN fly_votes.type_vote = "dislike" THEN 1 ELSE 0 END) as dislikes_count')
+//     )
+//     ->groupBy('flies.id', 'flies.title', 'flies.description', 'flies.created_at', 'flies.updated_at')  // Incluindo todas as colunas
+//     ->paginate(10);
+
+
         return view('flies.index', compact('flies'));
     }
 
